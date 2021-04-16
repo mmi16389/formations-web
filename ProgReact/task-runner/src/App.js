0a1d7da68aeaa85ecb3516ae9030f50581c9fac4
Maryvon MICHEL <maryvonmichel@mac-mini-de-maryvon.home>
2021-04-16 18:37:20 +0200

    adding react app

---
+++ ProgReact/task-runner/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Header } from './components/layout'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Header/>
+      </div>
+    </>
+  );
+}
+
+export default App;
